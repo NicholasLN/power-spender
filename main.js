dffdb001b79a37bf3d012fb2fc9274cfcaf5f60c
PhilScott <PhilScott4USA@gmail.com>
2022-10-04 04:35:53 -0400

    updoot

--- main.js
+++ main.js
@@ -32,16 +32,36 @@ figlet("POWER SPENDER", function (err, data) {
   console.log(chalk.whiteBright("By Phil Scott\n-------------"));
   prompt.start();
   prompt.get(schema, async function (err, result) {
-    await createSession(result.username, result.password);
-    console.log(chalk.greenBright("Session valid! Waiting for :01"));
-    const adsPerHour = parseInt(result.ads);
-    cron.schedule("1 * * * *", async () => {
-      for (let i = 0; i < adsPerHour; i++) {
-        await ad();
-      }
-      var time = new Date().toLocaleTimeString();
-      var str = chalk`{rgb(128,128,128).bold [${time}]}`;
-      console.log(str + chalk.whiteBright(" Spent, waiting another hour."));
-    });
+    const hasSession = await createSession(result.username, result.password);
+    if (hasSession) {
+      console.log(chalk.greenBright("Session valid! Waiting for :01"));
+      const adsPerHour = parseInt(result.ads);
+      cron.schedule("1 * * * *", async () => {
+        for (let i = 0; i < adsPerHour; i++) {
+          await ad();
+        }
+        var time = new Date().toLocaleTimeString();
+        var str = chalk`{rgb(128,128,128).bold [${time}]}`;
+        console.log(str + chalk.whiteBright(" Spent, waiting another hour."));
+      });
+    } else {
+      console.log(
+        chalk.redBright(
+          "Application closing in 5 seconds because you're an idiot who doesn't know their own login credentials!"
+        )
+      );
+
+      var timeleft = 5;
+      var countdown = setInterval(function () {
+        if (timeleft <= 0) {
+          clearInterval(countdown);
+        }
+        if (timeleft != 0) {
+          console.log(chalk.redBright(timeleft + "..."));
+        }
+        timeleft -= 1;
+      }, 1000);
+      return;
+    }
   });
 });
